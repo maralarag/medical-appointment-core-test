fecd0997f646935ffca48d5e637d63bb1de09829
marianalarag <mariana.larar@ogum.com.mx>
2026-03-04 10:31:19 -0600

    Trigger review after branch protection

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware([
         // CRUD de Doctores
         Route::resource('doctors', DoctorController::class);
 
-        // Rutas de soporte
+        // Rutas de soporte (Tickets)
         Route::resource('tickets', TicketController::class);
     });
 });
